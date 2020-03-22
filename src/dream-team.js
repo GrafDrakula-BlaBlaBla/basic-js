b550a6cc58a6d6d66d4b741d835eedf15b2ea499
GrafDrakula-BlaBlaBla <bogdanlevchenko11.l@gmail.com>
2020-03-22 17:45:42 +0300

    feat: add solution dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,33 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(!Array.isArray(members)) {
+    return false;
+  }
+  let i = [];
+  members.forEach((elem) => {
+debugger
+    if(typeof elem === `string`){
+      i.push(elem.match(/\b\w/i));
+    }
+  })
+
+  let n = i.sort().map(el => el[0].toUpperCase()).join('');
+
+  return n;
 };
+
+// const members = [
+//   '   William Alston ',
+//   ' Paul Benacerraf',
+//   '  Ross Cameron',
+//   '       Gilles Deleuze',
+//   '  Arda Denkel ',
+//   '  Michael Devitt',
+//   '  Kit Fine',
+//   ' Nelson Goodman',
+//   'David Kolb',
+//   '   Saul Kripke',
+//   '  Trenton Merricks',
+//   '  Jay Rosenberg',
+// ];
+
+// createDreamTeam(members);
