5f847d5c926e71642b1936e6867650bdac3027a6
GrafDrakula-BlaBlaBla <bogdanlevchenko11.l@gmail.com>
2020-03-22 23:15:00 +0300

    feat:add solutions recursive-depth extended-repeater transform-array

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,83 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options ) {
+    if (typeof str !== 'string') {
+        str = String(str);
+    }
+    if (typeof options.addition !== 'string') {
+        options.addition = String(options.addition);
+    }
+
+     if(typeof options.separator == 'undefined') {
+        options.separator = '+';
+    }
+    if(typeof options.additionSeparator == 'undefined') {
+        options.additionSeparator = '|';
+    }
+    if(typeof options.addition == 'undefined'){
+        options.addition = '';
+    }
+    if(options.addition == null){
+        options.addition = String(null);
+    }
+    if(options.repeatTimes == undefined){
+        options.repeatTimes = '0';
+    }
+    if(options.additionRepeatTimes == undefined){
+        options.additionRepeatTimes = '0';
+    }
+
+     let separatorLength;
+    if(options.separator !== undefined) {
+        separatorLength = String(options.separator).length;
+    }
+    let additionSeparator;
+    if(options.additionSeparator !== undefined) {
+        additionSeparator = String(options.additionSeparator).length;
+    }
+
+     let additionStr;
+    if(options.addition == '' || additionSeparator == ''){
+        additionStr = '';
+    } else {
+        additionStr = options.addition.concat(String(options.additionSeparator));
+    }
+
+     let repeatAdditionStr;
+    if(additionStr == '') {
+        repeatAdditionStr = '';
+    }
+    else if(additionStr == '' && additionRepeatTimes == '') {
+        repeatAdditionStr = '';
+    } else {
+        repeatAdditionStr = additionStr.repeat(options.additionRepeatTimes);
+    }
+
+     let separatorAdditionStr;
+    if(repeatAdditionStr == '' || additionSeparator == '' || repeatAdditionStr.length == 0) {
+        separatorAdditionStr = '';
+    } else {
+        separatorAdditionStr = repeatAdditionStr.substr(0, repeatAdditionStr.length - additionSeparator);
+    }
+
+     let concatStr;
+    if(options.repeatTimes == '0') {
+        concatStr = str.concat(additionStr);
+    } else { 
+        concatStr = str.concat(separatorAdditionStr + String(options.separator));
+    }
+
+     let repeatStr;
+    if(options.repeatTimes == '0') {
+        repeatStr = concatStr;
+    } else {
+        repeatStr = concatStr.repeat(options.repeatTimes);
+    }
+
+     let separatorStr;
+    if(options.repeatTimes == '0') {
+        separatorStr = repeatStr.substr(0, repeatStr.length - additionSeparator);
+    } else { 
+        separatorStr = repeatStr.substr(0, repeatStr.length - separatorLength);
+    }
+    return separatorStr;
 };
   
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,63 @@
+let maxDepth = 0;
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
+    calculateDepth(arr, depth = 1) {
+        
+        for (let i = 0; i < arr.length; i++) {
+          if(Array.isArray(arr[i])) maxDepth = this.calculateDepth(arr[i], depth + 1);
+        }
+        maxDepth = (maxDepth > depth) ? maxDepth : depth;
+      
+        let result = maxDepth;
+        maxDepth = 0;
+        return result;
+        }
+
+
+
+
+    // calculateDepth(arr,level = 1) {
+        
+    //     arr.forEach(elem => {
+    //          if(Array.isArray(elem)) {
+    //              let f = this.calculateDepth(elem, level);
+    //              if(level < f) {
+    //                 level ++;
+    //              }
+    //          } else {
+    //              return 0;
+    //          }
+    //      })
+    //     return level + 1;
+    // }
 };
+// If(level <  this.calculateDepth(elem, level){
+//     level = this.calculateDepth(elem, level);
+// }
+
+// if(!level) {
+        //     level = 1;
+        // }
+        
+        // let res = arr.map(elem => {
+        //     if (Array.isArray(elem)) {
+        //         return this.calculateDepth(elem);
+        //     } else {
+        //         return 0;
+        //     }
+        // }, this);
+
+        // res = Math.max(...res) + 1;
+        // return res;
+
+
+// calculateDepth(arr) {
+//     let depth = 0;
+//     for (const item of arr) {
+//       depth = Array.isArray(item)
+//         ? Math.max(this.calculateDepth(item), depth)
+//         : depth;
+//     }
+//     return ++depth;
+//   }
+
+
